8073d86789d1f2ee92499aee7d6443fa337f8747
Abhinav Thakur <abhinavthakur@Abhinavs-Air.lan>
2024-11-07 11:43:05 +0530

    Cleaned up deleted files and modifications

--- Week2/src/main/java/com/week2/practice/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.week2.practice.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
